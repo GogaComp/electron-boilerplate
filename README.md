28bd9525ab1db2e44dd8fe97c09a68928652b648
GogaComp <goga789goga@gmail.com>
2020-09-24 15:43:42 +0300

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# Electron boileplate
+
+Electron boileplate with webpack (babel, sass).
+
+## Installation
+
+```javascript
+$ npm i
+```
+
+or
+
+```javascript
+$ yarn
+```
+
+## Usage
+
+### Start electron:
+
+```javascript
+$ npm start
+```
+
+or
+
+```javascript
+$ yarn start
+```
+
+### Build webpack:
+
+```javascript
+$ npm run build
+```
+
+or
+
+```javascript
+$ yarn build
+```
+
+### Generate executable build:
+
+```javascript
+$ npm run dist
+```
+
+or
+
+```javascript
+$ yarn dist
+```
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
+  MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/web"),
+    filename: "js/[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin({}),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+  ],
+};
